363dc09f47e33fab72e3d63d8d19af8253ccdf33
Michael Deer <mick.deer@gmail.com>
2017-09-12 11:33:08 +0000

    Updated message

--- server.js
+++ server.js
@@ -13,11 +13,10 @@ var middleware = {
 	}
 };
 
-//app.use(middleware.requireAuthentication)
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('About Us');
+	res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
